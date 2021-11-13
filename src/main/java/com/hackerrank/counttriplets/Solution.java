ef05d05cb1f4da1fe8a48d731cc82c5f87d0cc42
pbalachandran <pbalachandranresume@gmail.com>
2021-11-13 14:51:39 -0600

    Space

--- src/main/java/com/hackerrank/counttriplets/Solution.java
+++ src/main/java/com/hackerrank/counttriplets/Solution.java
@@ -15,7 +15,6 @@ import static java.util.stream.Collectors.toList;
 
 public class Solution {
 
-
     static long countTriplets(List<Long> arr, long r) {
         List<Long> list = new ArrayList<>();
         for (Long element : arr) {
