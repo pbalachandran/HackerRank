83b62c1717da314d89bdf81e013b7dd2025c62de
pbalachandran <pbalachandranresume@gmail.com>
2021-12-31 12:15:51 -0600

    nearest clone

---
+++ src/main/java/com/hackerrank/nearestclone/Solution.java
@@ -0,0 +1,167 @@
+package com.hackerrank.nearestclone;
+
+import sun.jvm.hotspot.gc.shared.Generation;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the findShortest function below.
+
+    /*
+     * For the unweighted graph, <name>:
+     *
+     * 1. The number of nodes is <name>Nodes.
+     * 2. The number of edges is <name>Edges.
+     * 3. An edge exists between <name>From[i] to <name>To[i].
+     *
+     */
+
+    public static class Node {
+        private boolean isVisited;
+        private int nodeNumber;
+        private long colorNumber;
+        private List<Node> connections = new ArrayList<>();
+
+        public Node(int nodeNumber, long colorNumber) {
+            this.nodeNumber = nodeNumber;
+            this.colorNumber = colorNumber;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return nodeNumber == node.nodeNumber &&
+                    colorNumber == node.colorNumber;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nodeNumber, colorNumber);
+        }
+    }
+
+    private static List<Node> adjMatrix = new ArrayList<>();
+    private static Map<Long, List<Node>> nodesByColor = new HashMap<>();
+    private static int shortestPath = -1;
+
+    static int findShortest(int graphNodes, int[] graphFrom, int[] graphTo, long[] ids, int val) {
+
+        buildGraph(graphNodes, ids, graphFrom, graphTo);
+
+        long targetColor = Long.valueOf(val);
+
+        List<Node> targetColorNodes = nodesByColor.get(targetColor);
+
+        if (targetColorNodes == null) {
+            return shortestPath;
+        }
+
+        Iterator<Node> iterator = targetColorNodes.iterator();
+        while (iterator.hasNext()) {
+            Node origin = iterator.next();
+            origin.isVisited = true;
+
+            Iterator<Node> edgeNodesIterator = origin.connections.iterator();
+            while (edgeNodesIterator.hasNext()) {
+                Node edgeNode = edgeNodesIterator.next();
+                edgeNode.isVisited = true;
+                recurse(edgeNode, targetColor, 1);
+                if (shortestPath == 1) {
+                    return shortestPath;
+                }
+            }
+        }
+        return shortestPath;
+    }
+
+    private static void recurse(Node node, long targetColor, int minPath) {
+        if (node.colorNumber == targetColor) {
+            if (shortestPath == -1 || minPath < shortestPath) {
+                shortestPath = minPath;
+            }
+        } else {
+            Iterator<Node> iterator = node.connections.iterator();
+            while (iterator.hasNext()) {
+                Node edgeNode = iterator.next();
+                if (!edgeNode.isVisited) {
+                    minPath++;
+                    edgeNode.isVisited = true;
+                    recurse(edgeNode, targetColor, minPath);
+                }
+            }
+        }
+    }
+
+    private static void buildGraph(int graphNodes, long[] ids, int[] graphFrom, int[] graphTo) {
+        for (int i = 0; i < graphNodes; i++) {
+            long color = ids[i];
+            Node node = new Node(i + 1, color);
+            adjMatrix.add(node);
+
+            if (nodesByColor.keySet().contains(color)) {
+                List<Node> nodes = nodesByColor.get(color);
+                nodes.add(node);
+            } else {
+                List<Node> nodes = new ArrayList<>();
+                nodes.add(node);
+                nodesByColor.put(color, nodes);
+            }
+        }
+
+        for (int j = 0; j < graphFrom.length; j++) {
+            int from = graphFrom[j];
+            int to = graphTo[j];
+
+            Node fromNode = adjMatrix.get(from - 1);
+            Node toNode = adjMatrix.get(to - 1);
+
+            fromNode.connections.add(toNode);
+            toNode.connections.add(fromNode);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] graphNodesEdges = scanner.nextLine().split(" ");
+        int graphNodes = Integer.parseInt(graphNodesEdges[0].trim());
+        int graphEdges = Integer.parseInt(graphNodesEdges[1].trim());
+
+        int[] graphFrom = new int[graphEdges];
+        int[] graphTo = new int[graphEdges];
+
+        for (int i = 0; i < graphEdges; i++) {
+            String[] graphFromTo = scanner.nextLine().split(" ");
+            graphFrom[i] = Integer.parseInt(graphFromTo[0].trim());
+            graphTo[i] = Integer.parseInt(graphFromTo[1].trim());
+        }
+
+        long[] ids = new long[graphNodes];
+
+        String[] idsItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < graphNodes; i++) {
+            long idsItem = Long.parseLong(idsItems[i]);
+            ids[i] = idsItem;
+        }
+
+        int val = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int ans = findShortest(graphNodes, graphFrom, graphTo, ids, val);
+
+        bufferedWriter.write(String.valueOf(ans));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
